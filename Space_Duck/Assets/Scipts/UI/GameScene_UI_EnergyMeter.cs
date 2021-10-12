ec7c4755dfdd3d4145b10012c83eea2faaeee9e3
Murdier <91130648+Murdier@users.noreply.github.com>
2021-10-12 15:57:42 +0300

    Added EnergyMeter

---
+++ Space_Duck/Assets/Scipts/UI/GameScene_UI_EnergyMeter.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScene_UI_EnergyMeter : MonoBehaviour
+{
+    [SerializeField]
+    private Slider energyMeter;
+
+    public void Init(int maxEnergy)
+    {
+        energyMeter.maxValue = maxEnergy;
+        StartCoroutine(Demostrate());
+    }
+
+    public void ShowValue(int value)
+    {
+        if (value > energyMeter.maxValue)
+            value = (int)energyMeter.maxValue;
+
+        energyMeter.value = value;
+    }
+
+    private IEnumerator Demostrate()
+    {
+        while (energyMeter.value < energyMeter.maxValue)
+        {
+            yield return new WaitForSeconds(2);
+
+            ShowValue((int)energyMeter.value + 10);
+        }
+    }
+}
