50581a9125344b41748c1d842773542520596ca8
Vapsiainen <59833410+Vapsiainen@users.noreply.github.com>
2021-10-22 04:04:16 +0300

    Aloitettu avaimen tiputus-toiminnallisuutta

--- Space_Duck/Assets/Scipts/Item.cs
+++ Space_Duck/Assets/Scipts/Item.cs
@@ -13,6 +13,19 @@ public class Item : MonoBehaviour
     public GameManager gm;
     public int keyId;
 
+    private GameObject player;
+
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        dropKey();
+    }
+
     private void OnTriggerStay(Collider other)
     {
         switch (this.tag)
@@ -40,5 +53,17 @@ public class Item : MonoBehaviour
                 break;
         }
         
+    }
+
+    private void dropKey()
+    {
+        if (Input.GetKey(KeyCode.F) && gm.carryingKey)
+        {
+            Instantiate(Resources.Load("Key"), player.transform.position + new Vector3(2, 1, 0), Quaternion.identity);
+            Debug.Log("Avain tiputettu!");
+            gm.carryingKey = false;
+            //GameObject droppedKey = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //droppedKey.transform.position = player.transform.position + new Vector3(2, 1, 0);
+        }
     }
 }
